bda72655686bbb50623dbf02af4058fa3de34906
jduda27 <56557345+jduda27@users.noreply.github.com>
2019-10-24 12:05:39 -0500

    update

--- StackAssignment/src/Infix2Postfix.java
+++ StackAssignment/src/Infix2Postfix.java
@@ -7,10 +7,43 @@ public class Infix2Postfix {
 	 * contains only single-digit operands, four binary operators (+, -, *, /) and
 	 * parentheses.
 	 */
-	public String in2P(String infix) {
+	public static String in2P(String infix) {
+		String noW = "";
+		String result ="";
+		StackArrayBased stack = new StackArrayBased();
+		//Removes all whitespace and creates a new string
+		for(int i = 0; i <infix.length(); i++) {
+			if(!Character.isWhitespace(infix.charAt(i))) {
+				noW += String.valueOf(infix.charAt(i));
+			}
+		}
+		char[] exp = new char[noW.length()];
+		//creates an array of single characters that are not empty space
+		for(int i =0; i < noW.length();i++) {
+				exp[i] = noW.charAt(i);
+				//System.out.println(exp[i]);
+		}
 		
-		
-		return "";
+		for (int i = 0; i < noW.length(); i++){
+			if ( precedence(exp[i]) == 1 || precedence(exp[i]) == 2 || exp[i]=='(') {
+				stack.push(String.valueOf(exp[i]));
+				//System.out.println(stack.peek());
+			}else if(exp[i]==')') {
+				while(stack.peek() != "("){
+					System.out.println(stack.peek());
+					result += stack.peek(); 
+					System.out.println(result);
+					stack.pop();
+				}
+			}else {
+				result += String.valueOf(exp[i]);
+			}
+		}
+		while(!stack.isEmpty()) {
+			result += String.valueOf(stack.peek());
+			stack.pop();
+		}
+		return result;
 	}
 	
 	
@@ -29,12 +62,13 @@ public class Infix2Postfix {
 	
 	public static void main(String[] args) {
 		// Read user's input, no space between each character.
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		String postfixExp = ""; 
+		//Scanner sc = new Scanner(System.in);
+		//String str = sc.next();
+		//String postfixExp = ""; 
 		//This isyour postfix result.
-		StackArrayBased stack = new StackArrayBased(); 
+		//StackArrayBased stack = new StackArrayBased(); 
 		//Create a new stack using StackArrayBased class.
 		//Your implementation of infix to postfix algorithm goes here.
+		System.out.println(in2P("a+b*(c+d)/e"));
 	}
 }
