36ee542d3b77d76cc4f9a993abdf3f4849d83b79
jduda27 <56557345+jduda27@users.noreply.github.com>
2019-10-24 09:54:35 -0500

    Init

---
+++ StackAssignment/src/Infix2Postfix.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Infix2Postfix {
+
+	/*
+	 * Convert infix expression to postfix expression by implementing a stack. Infix
+	 * contains only single-digit operands, four binary operators (+, -, *, /) and
+	 * parentheses.
+	 */
+	// precedence() method takes operator as input and return the precendence as an
+	// integer.
+	// Larger number indicates higher precedence.
+	public static int precedence (char ch) {
+		if (ch == '+' || ch == '-') {
+			return 1;}
+		else if (ch == '*' || ch == '/') {
+			return 2;
+			}
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// Read user's input, no space between each character.
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		String postfixExp = ""; 
+		//This isyour postfix result.
+		StackArrayBased stack = new StackArrayBased(); 
+		//Create a new stack using StackArrayBased class.
+		//Your implementation of infix to postfix algorithm goes here.
+	}
+}
